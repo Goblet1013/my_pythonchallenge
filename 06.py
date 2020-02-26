09eb7ada01ae822968e9019ee5f9515c8af13b2e
Goblet1013 <1483345609@qq.com>
2020-02-26 11:49:56 +0800

    1-6

--- 06.py
+++ 06.py
@@ -1,12 +1,16 @@
 import os
 import zipfile
 
-file = "C:\\Users\\goblet\\Desktop\\channel"
-channel = r"‪C:\Users\goblet\Desktop\channel.zip"
+folder = "C:\\Users\\goblet\\Desktop\\channel"
+channel = r"C:\Users\goblet\Desktop\channel.zip"
 index = "90052"
 fzip = zipfile.ZipFile(channel,'r')
 
 while True:
-    with open(file+"\\"+index+".txt") as f:
-        print(fzip.getinfo(index+".txt").comment,end="")
+    with open(folder + "\\" + index + ".txt") as f:
+        temp = fzip.getinfo(index+".txt").comment.decode('utf-8')
+        if temp == '\n':
+            print()
+        else:
+            print(temp, end='')
         index = f.read().split('nothing is ')[1]
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # my_pythonchallenge
 http://www.pythonchallenge.com/
-该网站把学习python语言解密化、游戏化，记录一下我的闯关代码
+该网站把学习python语言解密化、游戏化，记录一下我的闯关代码  
+01 有趣  
+02 有趣  
+03 左右只能有三个大写字母，意味着三个之外的必须是小写字母，不能只用[A-Z]{3}([a-z])[A-Z]{3}  
+04 有趣  
+05 没觉得pickle和peak hell像  
+06 完全没想到换成zip，而且遍历完文件后发现还是没有解决，  
+看了网上的提示才知道要用zipfile库提取信息，有意思的是最后得到的dockey不是答案，  
+要再用组成字母的小字母拼成单词oxygen----it's in the air，有趣  
